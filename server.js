46f19997df485cad66bb670befe0d65c42e3a3bb
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-08-17 16:15:50 +1000

    Changed order of /api/notes GET endpoint

--- server.js
+++ server.js
@@ -11,11 +11,6 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
-
 app.get('/api/notes', (req, res) => {
     fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
         if (err) {
@@ -25,6 +20,13 @@ app.get('/api/notes', (req, res) => {
         res.json(JSON.parse(data));
     });
 });
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
